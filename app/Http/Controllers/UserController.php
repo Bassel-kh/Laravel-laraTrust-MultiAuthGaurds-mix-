96b6a77da7602e7b6a3f70af6791b032d001792a
Bassel-kh <bassel.kh.svu@gmail.com>
2020-08-23 17:29:07 +0300

    #6 Edit Controller: To user Role Laratrust $ Migrate Admin and Customer ( Steps: 1, 2, 3, 4, 5, 6)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('role:user');
+
+    }
+
     public function index()
     {
         return view('user.index');
