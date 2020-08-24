fca0f3a1a3aee08b6e3eab7402facf75eb603232
Bassel-kh <bassel.kh.svu@gmail.com>
2020-08-24 18:41:09 +0300

    #12 Set up authentication pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,12 +5,15 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header"> {{ isset($url) ? ucwords($url) : ""}} {{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
+                    @isset($url)
+                    <form method="POST" action='{{ url("login/$url") }}' aria-label="{{ __('Login') }}">
+                    @else
+                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                    @endisset
+                    @csrf
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -70,4 +73,5 @@
         </div>
     </div>
 </div>
+
 @endsection
