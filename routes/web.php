06242e8b05a3a66f0acd7e2a12844d4c3a3c8276
Bassel-kh <bassel.kh.svu@gmail.com>
2020-08-24 21:27:28 +0300

    #14 Set up the routes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,24 @@ Route::get('/', function () {
 
 Auth::routes();
 
+// Begin laratrust /////////////////////////////////////////////////////////////
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/admin', 'AdminController@index')->name('admin');
-Route::get('/user', 'UserController@index')->name('user');
+//    Route::get('/home', 'HomeController@index')->name('home');
+//    Route::get('/admin', 'AdminController@index')->name('admin');
+//    Route::get('/user', 'UserController@index')->name('user');
+
+// End laratrust //////////////////////////////////////////////////////////////////
+
+Route::get('/login/admin', 'Auth\LoginController@showAdminLoginForm');
+Route::get('/login/customer', 'Auth\LoginController@showCustomerLoginForm');
+Route::get('/register/admin', 'Auth\RegisterController@showAdminRegisterForm');
+Route::get('/register/customer', 'Auth\RegisterController@showCustomerRegisterForm');
+
+Route::post('/login/admin', 'Auth\LoginController@adminLogin');
+Route::post('/login/customer', 'Auth\LoginController@CustomerLogin');
+Route::post('/register/admin', 'Auth\RegisterController@createAdmin');
+Route::post('/register/customer', 'Auth\RegisterController@createCustomer');
+
+Route::view('/home', 'home')->middleware('auth');
+Route::view('/admin', 'admin');
+Route::view('/customer', 'customer')->middleware('auth:customer');
