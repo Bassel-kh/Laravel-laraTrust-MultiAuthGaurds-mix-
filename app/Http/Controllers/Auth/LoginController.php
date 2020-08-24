3475a5d0459843acc146b81610abbd7ad6445f85
Bassel-kh <bassel.kh.svu@gmail.com>
2020-08-24 13:55:32 +0300

    #11 Set up the controllers ( Steps: 1- Modify LoginController , 2- Modify RegisterController )

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+/////////////////////////////
+use Illuminate\Http\Request;
+use Auth;
+/// ///////////////////////
 
 class LoginController extends Controller
 {
@@ -34,7 +38,60 @@ class LoginController extends Controller
      * @return void
      */
     public function __construct()
-    {
+    {   //     We set the middleware to restrict access to this controller or its methods.
+        //          It is important we defined all the different types of guests in the controller.
+        //              This way, if one type of user is logged in and you try to use another user type to log in,
+        //                   it will redirect you to a predefined authentication page.
+
         $this->middleware('guest')->except('logout');
+        $this->middleware('guest:admin')->except('logout');
+        $this->middleware('guest:customer')->except('logout');
     }
+    ##########################################################
+    ////////////////// Begin Admin Methods ///////////////////
+    public function showAdminLoginForm()
+    {
+        return view('auth.login', ['url' => 'admin']);
+    }
+
+    public function adminLogin(Request $request)
+    {
+        $this->validate($request, [
+            'email'   => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
+
+            return redirect()->intended('/admin');
+        }
+        return back()->withInput($request->only('email', 'remember'));
+    }
+
+    ////////////////// End Admin Methods ///////////////////
+    #########################################################
+    #########################################################
+    ////////////////// Begin Customer Methods /////////////////
+    public function showCustomerLoginForm()
+    {
+        return view('auth.login', ['url' => 'customer']);
+    }
+
+    public function customerLogin(Request $request)
+    {
+        $this->validate($request, [
+            'email'   => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        if (Auth::guard('customer')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
+
+            return redirect()->intended('/customer');
+        }
+        return back()->withInput($request->only('email', 'remember'));
+    }
+    ////////////////// End Customer Methods /////////////////
+    /// ###############################################  ////
+
+
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+/////////////////////////
+use App\User;
+use App\Admin;
+use App\Customer;
+use Illuminate\Http\Request;
+////////////////////////
 
 class RegisterController extends Controller
 {
@@ -39,7 +44,62 @@ class RegisterController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+        $this->middleware('guest:admin');
+        $this->middleware('guest:customer');
     }
+
+
+    ////////////////// Begin Admin Methods ///////////////////
+
+    public function showAdminRegisterForm()
+    {
+        return view('auth.register', ['url' => 'admin']);
+    }
+
+    ##############################
+    //methods for creating an admin:
+    protected function createAdmin(Request $request)
+    {
+        $this->validator($request->all())->validate();
+        $admin = Admin::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+        return redirect()->intended('login/admin');
+    }
+
+
+    ##################################
+
+    ////////////////// End Admin Methods ///////////////////
+    ///  ###############################################   ///
+
+
+    ////////////////// Begin Customer Methods ///////////////////
+
+    public function showCustomerRegisterForm()
+    {
+        return view('auth.register', ['url' => 'customer']);
+    }
+
+    ##############################
+    //methods for creating an Customer:
+    protected function createCustomer(Request $request)
+    {
+        $this->validator($request->all())->validate();
+        $customer = Customer::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+        return redirect()->intended('login/customer');
+    }
+    ////////////////// End Customer Methods ///////////////////
+    ///  ###############################################   ///
+
+
+    ////////////////// Begin Validation Methods ///////////////
 
     /**
      * Get a validator for an incoming registration request.
@@ -51,23 +111,14 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users', 'unique:admins', 'unique:customers'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
+    ////////////////// End Validation Methods ///////////////////
+    /// ################################################   ///
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
-    }
+
+
+
 }
